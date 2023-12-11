b5859ea4f896be664aa1b1ce5409549f677be937
binhdv2002 <binhsctn12345@gmail.com>
2023-12-11 20:12:12 +0700

    add category service api

---
+++ client/src/api/services/category.ts
@@ -0,0 +1,13 @@
+import CONFIG_KEYS from "../../config";
+import api from "../middlewares/protected-interceptor";
+
+export const addCategoryService = async (
+    endpoint: string,
+    categoryInfo: { name: string; description: string }
+) => {
+    const response = await api.post(
+        `${CONFIG_KEYS.API_BASE_URL}/${endpoint}`,
+        categoryInfo
+    );
+    return response;
+};
